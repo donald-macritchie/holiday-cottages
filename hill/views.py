1599f406bc35a51fc8cd0b6d77d310d56ac5573e
donald-macritchie <>
2023-10-06 14:23:22 +0000

    edit homestead_cottage view and add amenities to it. create for loop in homestead_cottage.html to display

--- hill/views.py
+++ hill/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cottage, CottageImages, Amenities
 
 
@@ -6,5 +6,19 @@ def index(request):
     return render(request, "index.html")
 
 
+# def homestead_cottage(request):
+#     amenities = homestead_cottage.amenities.all()
+#     return render(request, 'homestead_cottage.html')
+
+
 def homestead_cottage(request):
-    return render(request, 'homestead_cottage.html')
+    homestead_cottage = get_object_or_404(Cottage, name='Homestead')
+
+    amenities = homestead_cottage.amenities.all()
+
+    context = {
+        'homestead_cottage': homestead_cottage,
+        'amenities': amenities,
+    }
+
+    return render(request, 'homestead_cottage.html', context)
