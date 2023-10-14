d60c276f02a2d42057f13ee91d2a5105bc240385
donald-macritchie <>
2023-10-14 13:11:07 +0000

    restyle index.html for welcome section

--- hill/urls.py
+++ hill/urls.py
@@ -1,16 +1,12 @@
 from . import views
 from django.urls import path
 
-
-app_name = "hill"
-
 urlpatterns = [
     path('', views.index, name="index"),
     path('homestead-cottage/', views.homestead_cottage,
          name="homestead_cottage"),
     path('marketview-cottage/', views.marketview_cottage,
          name="marketview_cottage"),
-    path('contact/', views.ContactMessage.as_view(),
-         name="contact"),
-    path('success/', views.ContactSuccessView.as_view(), name="success"),
+    path('contact/', views.ContactMessage.as_view(), name='contact'),
+    path('success/', views.ContactSuccessView.as_view(), name='success'),
 ]
