3960a00049c1d1779ebe9cda65061cfbd19f9cc0
donald-macritchie <>
2023-10-17 14:29:36 +0000

    refactor homestead and marketview views into one

--- hill/forms.py
+++ hill/forms.py
@@ -7,15 +7,8 @@ from .models import Booking, ContactMessage
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = [
-            'user',
-            'cottage',
-            'check_in_date',
-            'check_out_date',
-            'number_of_guests',
-            'guest_name',
-        ]
-
+        fields = ['check_in_date', 'check_out_date',
+                  'number_of_guests', 'guest_name']
 
 class ContactMessageForm(forms.ModelForm):
     class Meta:
--- hill/views.py
+++ hill/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Cottage, CottageImages, Amenities, ThingsToKnow, ThingsToDo
+from .models import Cottage, CottageImages, Amenities, ThingsToKnow, ThingsToDo, Booking
 from .forms import BookingForm, ContactMessageForm
+from django.contrib.auth.decorators import login_required
 from django.views.generic import FormView, TemplateView
 from django.urls import reverse_lazy
 import os
@@ -9,42 +11,36 @@ import os
 def index(request):
     return render(request, "index.html")
 
+def cottage_view(request, cottage_name):
+    cottage = get_object_or_404(Cottage, name=cottage_name)
+    description = cottage.description
 
-# Homestead Cottage
-def homestead_cottage(request):
-    homestead_cottage = get_object_or_404(Cottage, name='Homestead')
-    description = homestead_cottage.description
-
-    # import photos of homestead
-
-    images = CottageImages.objects.filter(cottage=homestead_cottage)
-    homestead_image = CottageImages.objects.get(title='house_sign_1')
-
+    # Import photos of the cottage
+    images = CottageImages.objects.filter(cottage=cottage)
+    cottage_image = CottageImages.objects.get(title='house_sign_1')
 
     # Amenities
-    amenities = homestead_cottage.amenities.all()
+    amenities = cottage.amenities.all()
     amenities_by_category = {}
-
     for category, _ in Amenities.CATEGORY_CHOICES:
         amenities_by_category[category] = amenities.filter(category=category)
 
     # Things to Know
     things_to_know_by_category = {}
-    things_to_know = homestead_cottage.things_to_know.all()
-
+    things_to_know = cottage.things_to_know.all()
     for category, _ in ThingsToKnow.CATEGORY_CHOICES:
         things_to_know_by_category[category] = things_to_know.filter(
             category=category)
 
-    no_of_bedrooms = homestead_cottage.no_of_bedrooms
-    no_of_bathrooms = homestead_cottage.no_of_bathrooms
+    no_of_bedrooms = cottage.no_of_bedrooms
+    no_of_bathrooms = cottage.no_of_bathrooms
 
     # BookingForm
     booking_form = BookingForm()
 
     content = {
-        'homestead_cottage': homestead_cottage,
-        'images' : images,
+        'cottage': cottage,
+        'images': images,
         'amenities_by_category': amenities_by_category,
         'description': description,
         'GOOGLEMAPS_API_KEY': os.environ.get('GOOGLEMAPS_API_KEY', ''),
@@ -52,44 +48,14 @@ def homestead_cottage(request):
         'no_of_bedrooms': no_of_bedrooms,
         'no_of_bathrooms': no_of_bathrooms,
         'booking_form': booking_form,
-        'homestead_image_url': homestead_image.image.url,
+        'cottage_image_url': cottage_image.image.url,
     }
 
-    return render(request, 'homestead_cottage.html', content)
+    return render(request, ['homestead_cottage.html', 'marketview_cottage.html'], content)
 
 
-# Marketview Cottage
-def marketview_cottage(request):
-    marketview_cottage = get_object_or_404(Cottage, name='Marketview')
-    description = marketview_cottage.description
+# Booking Section
 
-    amenities = marketview_cottage.amenities.all()
-    amenities_by_category = {}
-
-    for category, _ in Amenities.CATEGORY_CHOICES:
-        amenities_by_category[category] = amenities.filter(category=category)
-
-    things_to_know_by_category = {}
-    things_to_know = marketview_cottage.things_to_know.all()
-
-    for category, _ in ThingsToKnow.CATEGORY_CHOICES:
-        things_to_know_by_category[category] = things_to_know.filter(
-            category=category)
-
-    no_of_bedrooms = marketview_cottage.no_of_bedrooms
-    no_of_bathrooms = marketview_cottage.no_of_bathrooms
-
-    content = {
-        'marketview_cottage': marketview_cottage,
-        'amenities_by_category': amenities_by_category,
-        'description': description,
-        'GOOGLEMAPS_API_KEY': os.environ.get('GOOGLEMAPS_API_KEY', ''),
-        'things_to_know_by_category': things_to_know_by_category,
-        'no_of_bedrooms': no_of_bedrooms,
-        'no_of_bathrooms': no_of_bathrooms,
-    }
-
-    return render(request, 'marketview_cottage.html', content)
 
 
 # ContactMessage
