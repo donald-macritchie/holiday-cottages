87bf90cdc856c850247604fe921683755ae9aa23
donald-macritchie <>
2023-10-22 12:26:00 +0000

    implement django messages into base.html and style

--- holiday_cottages/settings.py
+++ holiday_cottages/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
     import env
 
@@ -30,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 GOOGLEMAPS_API_KEY = os.environ.get('GOOGLEMAPS_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-donald-macritchie-holida-klnjz8fej0.us2.codeanyapp.com',
                  'holiday-cottages-d0c0525a5440.herokuapp.com']
@@ -61,6 +63,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
