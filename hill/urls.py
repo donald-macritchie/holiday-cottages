a6ab8a311c68b39bcaea985e6c68e2a0205282fb
donald-macritchie <>
2023-10-23 11:01:39 +0000

    delete user_profile.html and include the content into logout.html. Rename to profile.html

--- hill/urls.py
+++ hill/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('success/', views.ContactSuccessView.as_view(), name='success'),
     path('things_to_do/', views.things_to_do, name='things_to_do'),
     path('booking/', views.booking, name='book_cottage'),
-    path('profile/', views.user_profile, name='user_profile'),
 ]
