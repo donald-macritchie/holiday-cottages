579a26a68bd878ba8e64ed830b9df9dd7f684b2d
donald-macritchie <>
2023-10-27 14:50:00 +0000

    fix typo in settings, deploy

--- holiday_cottages/settings.py
+++ holiday_cottages/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
