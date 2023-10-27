b8f87701958b0b6497eac1eb0191e0d97cdab7e3
donald-macritchie <>
2023-10-27 14:27:54 +0000

    set xframeoptions

--- holiday_cottages/settings.py
+++ holiday_cottages/settings.py
@@ -34,10 +34,10 @@ GOOGLEMAPS_API_KEY = os.environ.get('GOOGLEMAPS_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8003-donald-macritchie-holida-klnjz8fej0.us2.codeanyapp.com',
-                 'holiday-cottages-d0c0525a5440.herokuapp.com']
+                 'holiday-cottages-d0c0525a5440.herokuapp.com/']
 
 
 # Application definition
